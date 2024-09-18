7ce9d85e142c7971c620b00cafc1aaa9575ac1d4
IdoCohen7 <idocohen727@gmail.com>
2024-09-18 19:34:06 +0300

    section 6 continue
    finished basic CRUD routes

--- app.js
+++ app.js
@@ -18,13 +18,36 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// Get route
+// GET route
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
       tours: tours,
+    },
+  });
+});
+
+// GetTour route
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1; // Multiply numeric string to turn it into a number
+  const tour = tours.find((el) => el.id === id);
+  console.log(tour);
+
+  // No tour found
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  // tour found
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
     },
   });
 });
@@ -47,6 +70,47 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1; // Multiply numeric string to turn it into a number
+  const tour = tours.find((el) => el.id === id);
+  console.log(tour);
+
+  // No tour found
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  // tour found, PATCH logic not implemented
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Tour is updated!',
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1; // Multiply numeric string to turn it into a number
+  const tour = tours.find((el) => el.id === id);
+
+  // No tour found
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+
+  // tour found, PATCH logic not implemented, 204 == no content
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 // start server
