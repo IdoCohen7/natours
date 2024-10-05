12d361541dee7a3659cdcd61759a85ec384c5c49
IdoCohen7 <idocohen727@gmail.com>
2024-10-05 16:10:42 +0300

    lect 147
    finished implemented all security middlewares

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const { xss } = require('express-xss-sanitizer');
+const hpp = require('hpp');
 const app = express(); // MIDDLEWARE
 
 const AppError = require('./utils/appError');
@@ -8,15 +12,55 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-// ONLY USE LOGGER MIDDLEWARE IF WE ARE IN DEVELOPMENT
+// GLOBAL MIDDLEWARE - SECURITY HTTP HEADERS
+app.use(helmet());
+
+// GLOBAL MIDDLEWARE - ONLY USE LOGGER MIDDLEWARE IF WE ARE IN DEVELOPMENT
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json()); // MIDDLE WARE BODY PARSER
-app.use(express.static(`${__dirname}/public`)); // ACCESS HTML
+// GLOBAL MIDDLEWARE - RATE LIMITER LEARNED ON SECURITY SECTION
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // ONE HOUR WINDOW
+  message: 'too many requests from this IP, please try again in an hour.',
+});
+
+app.use('/api', limiter);
+
+// GLOBAL MIDDLEWARE - BODY PARSER
+app.use(
+  express.json({
+    limit: '10kb',
+  }),
+);
+
+// GLOBAL MIDDLEWARE - PROVOKE NOSQL QUERIES INJECTION INTO BODY
+app.use(mongoSanitize());
+
+// GLOBAL MIDDLEWARE - FILTER OUT HARMFUL SCRIPTS FROM USER'S INPUT INTO REQUESTS
+app.use(xss());
+
+// GLOBAL MIDDLEWARE - PREVENT HTTP PARAMETER POLLUTION
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingCount',
+      'ratingAvg',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+// GLOBAL MIDDLEWARE - SERVING STATIC FILES (ACCESS HTML)
+app.use(express.static(`${__dirname}/public`));
+
+// GLOBAL MIDDLEWARE - ADD TIMESTAMP FOR EACH REQUEST
 app.use((req, res, next) => {
-  // MIDDLEWARE OF OUR CHOICE - ADD TIMESTAMP FOR EACH REQUEST
   req.requestTime = new Date().toISOString();
   next();
 });
